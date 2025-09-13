01e5640754da65c4c608a2cdb7cc2485d9904fc2
Fahad Malik <malikfahad1947@gmail.com>
2025-09-13 07:28:46 +0500

    Added simple calculator project

---
+++ simple_calculator.py
@@ -0,0 +1,46 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Division by zero."
+
+
+print("Welcome to the Calculator! ✨")
+print("Available operations: +, -, *, /")
+
+
+while True:
+    x=   float(input ("Enter the value of x = "))
+    op= input("Enter the oprator i.e + , -, * , / = ")
+    y=   float(input( "Enter the value of y = "))
+
+    if op=="+" :
+        print("Result : " , add(x,y))
+    elif op=="-":
+        print("Result : " , subtract(x,y))
+    elif op=="*":
+        print("Result : " , multiply(x,y))
+    elif op=="/":
+        if y==0:
+            print("Error! Division by zero")
+        else:
+            print("Result : " , divide(x,y))
+    else:
+        print("Invalid Operation")
+    
+
+    again = input("Do you want to calculate again? (y/n): ")
+    if again != 'y':
+            break
+
+
+
